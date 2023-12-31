f44411305366e3b542f990d4da6f36ec3054cfab
farhan7479 <farhan078609@gmail.com>
2023-12-31 15:54:59 +0530

    first commit#

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,8 @@ export default function App() {
             setOpen(!open);
           }}
         >
-          <img src="https://imgs.search.brave.com/3eU9zAsgrUY5aD-30LIPFHLu9SkDfRyu6umTO12r2Ks/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9sb2dv/ZGl4LmNvbS9sb2dv/LzE5NTUwMDEucG5n" alt="Logo" className="logo" />
-        </button>
+          <img src="https://imgs.search.brave.com/lsXiraFpiBLGY1F-Isnt9azZ56mpZv3X5ijNKFEMiaE/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZs/YXRpY29uLmNvbS8x/MjgvMTI4NzIvMTI4/NzI0MjQucG5n" alt="Logo" className="logo" />
+      </button>
       </div>
     </>
   );
